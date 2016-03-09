73eb74375468bb3ca50716354186686eb545d488
frogcat <frogcat@users.noreply.github.com>
2016-03-09 16:57:31 +0900

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+rm -rf out || exit 0;
+
+mkdir -p out;
+
+bash test.sh
+
+# go to the out directory and create a *new* Git repo
+cd out
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "frogcat@users.noreply.github.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+# git push "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
---
+++ main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var request = require('request');
+var fs = require('fs');
+
+var dst = process.argv[2];
+var src = process.argv[3];
+
+if (!dst || !src)
+  process.exit(1);
+
+function solo(json) {
+  (json.layers || json.entries || []).forEach(solo);
+  if (json.id)
+    fs.writeFileSync(dst + '/' + json.id + ".txt", JSON.stringify(json, null, '  '));
+}
+
+function tree(json, dir) {
+  var index = 0;
+  (json.layers || (json.forEach ? json : [])).forEach(function(v, i) {
+    if (v.entries) {
+      index++;
+      var child = dir + '/' + index;
+      fs.mkdirSync(child);
+      tree(v.entries, child);
+      v.entries = "./" + index + "/0.txt";
+    }
+  });
+  fs.writeFileSync(dir + '/0.txt', JSON.stringify(json, null, '  '));
+
+}
+
+request.get({
+  url : src
+}, function(error, response, txt) {
+
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+  while (txt.indexOf("{") != 0)
+    txt = txt.substr(1);
+  var json = JSON.parse(txt);
+  solo(json);
+
+  var dir = dst + "/" + src.replace(/^.*\//, "").replace(/\..*$/, "");
+  fs.mkdirSync(dir);
+  tree(json, dir);
+});
