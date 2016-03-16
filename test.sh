54b20a9e1aba05a0e1867b2e697bffa52203997d
Yuzo Matsuzawa <frogcat@users.noreply.github.com>
2016-03-16 15:25:05 +0900

    Add layers5.txt

--- test.sh
+++ test.sh
@@ -7,5 +7,6 @@ node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers1.txt
 node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers2.txt
 node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers3.txt
 node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers4.txt
+node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers5.txt
 node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers_experimental.txt
 node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers_invisible.txt
