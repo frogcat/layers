f2a0e0bc780a95c20fdfa865ce2a9d21c5eb710f
Yuzo Matsuzawa <frogcat@users.noreply.github.com>
2016-04-18 18:41:08 +0900

    Switch layer source
    http://gsi-cyberjapan.github.io/gsimaps/ to http://maps.gsi.go.jp/

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 set -e
 
 mkdir -p out;
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers0.txt
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers1.txt
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers2.txt
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers3.txt
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers4.txt
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers5.txt
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers_experimental.txt
-node main.js out http://gsi-cyberjapan.github.io/gsimaps/layers_txt/layers_invisible.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers0.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers1.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers2.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers3.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers4.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers5.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers_experimental.txt
+node main.js out http://maps.gsi.go.jp/layers_txt/layers_invisible.txt
